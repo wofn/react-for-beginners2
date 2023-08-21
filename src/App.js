066df68586f3b6d79912a3b7add3430741dedbba
wofn <siny10188@naver.com>
2023-08-22 08:17:16 +0900

    코인 트래커
    +값을 작성하면 그 값보다 작은 종목만 보이게 하기
    + 보이는 종목의 수를 ()안에 나타내기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const [money, setMoney] = useState(0);
+  const onChange = (event) => {
+    setMoney(event.target.value);
+  };
+
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   useEffect(() => {
@@ -13,16 +18,35 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange}
+        type="number"
+        value={money}
+        placeholder="Write the money you have"
+      />
+
+      <h1>
+        The Coins!(
+        {
+          coins.filter((coin) =>
+            money === 0 ? true : coin.quotes.USD.price > money
+          ).length
+        }
+        )
+      </h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
         <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
+          {coins
+            .filter((coin) =>
+              money === 0 ? true : coin.quotes.USD.price > money
+            )
+            .map((coin) => (
+              <option>
+                {coin.name}({coin.symbol}): ${coin.quotes.USD.price} BTC
+              </option>
+            ))}
         </select>
       )}
     </div>
